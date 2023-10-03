bdc10d2b7d0647551bd9a2ec8b6e8a6346f2f015
Motty Harlig <harligmotty@gmail.com>
2023-10-03 18:02:13 -0400

    added setters for position and type

--- lib/models/positions.py
+++ lib/models/positions.py
@@ -12,6 +12,34 @@ class Position():
     def __repr__(self):
         return f"<{self.id}: Pos: {self.position}, Type: {self.type}>"
     
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        if isinstance(position, str) and len(position):
+            self._position = position
+        else:
+            raise ValueError(
+                "Position must be a non-empty string"
+            )
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        if isinstance(type, str) and len(type):
+            self._type = type
+        else:
+            raise ValueError(
+                "Type must be a non-empty string"
+            )
+    
+    
+    
     @classmethod
     def create_table(cls):
         """ Create a new table to persist the attributes of Position instances """
