442e9b493eae3525496eac073b170e677713fe4e
Motty Harlig <harligmotty@gmail.com>
2023-10-03 19:34:48 -0400

    added find by id method

--- lib/models/positions.py
+++ lib/models/positions.py
@@ -108,3 +108,15 @@ class Position():
         all_rows = [cls.instance_from_db(row) for row in rows] 
         return all_rows
     
+    @classmethod
+    def find_by_id(cls, id):
+        """Return a Position object corresponding to the table row matching the specified primary key"""
+        sql = """
+            SELECT *
+            FROM positions
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
