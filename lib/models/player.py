e62867337079a8e9d442811d5745a93f42661765
Motty Harlig <harligmotty@gmail.com>
2023-10-03 22:53:23 -0400

    added setter and getter methods for each value of class

--- lib/models/player.py
+++ lib/models/player.py
@@ -15,3 +15,67 @@ class Player():
             f"<Employee {self.id}: Name: {self.name}, #: {self.number}, Goals: {self.goals}, Assists: {self.assists} " +
             f"Position ID: {self.position_id}>"
         )
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
+    
+    @property
+    def number(self):
+        return self._number
+    
+    @number.setter
+    def number(self, number):
+        if isinstance(number, int) and len(number):
+            self._number = number
+        else:
+            raise ValueError(
+                "Number must be a number"
+            )
+    
+    @property
+    def goals(self):
+        return self._goals
+    
+    @goals.setter
+    def goals(self, goals):
+        if isinstance(goals, int) and len(goals):
+            self._goals = goals
+        else:
+            raise ValueError(
+                "Goals must be a number"
+            )
+    
+    @property
+    def assists(self):
+        return self._assists
+    
+    @assists.setter
+    def assists(self, assists):
+        if isinstance(assists, int) and len(assists):
+            self._assists = assists
+        else:
+            raise ValueError(
+                "Assists must be a number"
+            )
+    
+    @property
+    def position_id(self):
+        return self._position_id
+    
+    @position_id.setter
+    def position_id(self, position_id):
+        if type(position_id) is int and Position.find_by_id(position_id):
+            self._position_id = position_id
+        else:
+            raise ValueError(
+                "position_id must reference a department in the database")
